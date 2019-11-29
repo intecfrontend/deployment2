ae57f509958b4222eb53a11e76b8cbdba079e679
Ben <artegrafia.spain@gmail.commkdir>
2019-11-29 15:36:45 +0100

    Updates

---
+++ precache-manifest.e3d44b1b1d1796bd3fc3577a43c06271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc133f4d3ab34d7fe8cb875123d3c3a",
+    "url": "/deployment2/index.html"
+  },
+  {
+    "revision": "4cf59a3d882883e58558",
+    "url": "/deployment2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b81a8a861384393ae16",
+    "url": "/deployment2/static/js/2.e50112b1.chunk.js"
+  },
+  {
+    "revision": "4cf59a3d882883e58558",
+    "url": "/deployment2/static/js/main.3fc311f3.chunk.js"
+  },
+  {
+    "revision": "ef3de4e1ff347a9ab366",
+    "url": "/deployment2/static/js/runtime-main.1a34f34d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deployment2/static/media/logo.25bf045c.svg"
+  }
+]);
